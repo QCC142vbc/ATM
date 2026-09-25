df61e7702f04f03275d2ed5a4b2427523e08dae2
QCC142vbc <minis.patrik@gmail.com>
2026-09-25 20:35:53 +0200

    auth_service.py

---
+++ MiniATM/application/auth_service.py
@@ -0,0 +1,19 @@
+from domain.user import User
+
+class AuthService:
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
+    
+    def login(self, user_id: str, pin: str) -> User:
+        user = self.user_repository.get_by_id(user_id)
+
+        if user is None:
+            raise ValueError("User not found.")
+
+        for _ in range(self.MAX_ATTEMPTS):
+            if user.verify_pin(pin):
+                return user
+
+            # The CLI will handle asking for another PIN later.
+
+        raise ValueError("Maximum login attempts exceeded.")
